c561f59d593c85166a8f8fb699c643e745f3dc14
Eugene <ewackerbarth@gmail.com>
2021-01-31 13:43:58 -0800

    Added ability to find nearby buoys

--- BuoyDataUtilities.py
+++ BuoyDataUtilities.py
@@ -14,6 +14,7 @@ def constructBuoyDict():
     Outputs:
         buoysDict (dict): key = station ID, value = (lat, lon)
     '''
+
     # get and parse the active stations webpage
     activeStationsUrl = 'https://www.ndbc.noaa.gov/activestations.xml'
     ndbcPage = requests.get(activeStationsUrl)       #<class 'requests.models.Response'>
@@ -31,5 +32,90 @@ def constructBuoyDict():
         thisLat = buoy.get("lat")
         buoysDict[thisKey] = (thisLat, thisLon)
 
-
     return buoysDict
+
+
+def findNearbyBuoys(buoysDict, lat, lon, dLat, dLon):
+    '''
+    Trims the dictionary of active buoys based on the provided region of
+    interest.
+
+    Inputs:
+        buoysDict (dict): keys are station IDs and values are (lat, lon) tuples
+        lat (int/float): latitude
+        lon (int/float): longitude
+        dLat (int/float): half of latitude extent
+        dLon (int/float): half of longitude extent
+
+    Outputs:
+        nearbyBuoys (dict): only includes buoys in [lat-dLat, lat+dLat] and
+                          [lon-dLon, lon+dLon]
+    '''
+
+    # check for invalid inputs
+    if lat < -90 or lat > 90:
+        print('latitude must be > -90 and < 90 degrees')
+        return
+
+    if lon < -180 or lon > 180:
+        print('longitude must be > -180 and < 180 degrees')
+        return
+
+    if dLat < 0 or dLat > 90:
+        print('dLatitude must be > 0 and < 90 degrees')
+        return
+
+    if dLon < 0 or dLon > 180:
+        print('dLongitude must be > 0 and < 180 degrees')
+        return
+
+    # remove buoys that are not within the region of interest
+    nearbyBuoys = dict()
+    for key in buoysDict:
+        latLonTup = buoysDict[key]
+        # check latitude (-90, 90 deg)
+        latCheck = intervalCheck(lat, dLat, float(latLonTup[0]), 90)
+
+        # check longitude (-180, 180 deg)
+        lonCheck = intervalCheck(lon, dLon, float(latLonTup[1]), 180)
+        if latCheck and lonCheck:
+            nearbyBuoys[key] = (float(latLonTup[0]), float(latLonTup[1]))
+
+    print('# of buoys in region of interest = ' + str(len(nearbyBuoys)))
+    return nearbyBuoys
+
+
+def intervalCheck(x, dx, x0, halfRange):
+    '''
+    Returns True if x0 is within the interval [x-dx, x+dx], otherwise returns
+    false.
+
+    Inputs:
+        x (int/float): center of interval
+        dx (int/float): 1/2 of the extent of the interval
+        x0 (int/float): test point
+        halfRange (int/float):
+
+    Outputs:
+        boolean: True if x0 is within interval, false if not
+    '''
+
+    xMax = x + dx
+    xMin = x - dx
+
+    if x0 <= xMax and x0 >= xMin:
+        return True
+    else:
+        if xMax > halfRange: # interval of interest wraps around in + direction
+            if x0 < (xMax - 2*halfRange) and x0 > (xMin - 2*halfRange):
+                return True
+            else:
+                return False
+
+        elif xMin < -halfRange: # interval of interest wraps around in - direction
+            if x0 < (xMax + 2*halfRange) and x0 > (xMin + 2*halfRange):
+                return True
+            else:
+                return False
+        else:
+            return False
--- TestingUtilities.py
+++ TestingUtilities.py
@@ -1,11 +1,19 @@
 # Testing Utilities
 
 from BuoyDataUtilities import constructBuoyDict
+from BuoyDataUtilities import findNearbyBuoys
 
 buoysDict = constructBuoyDict()
 
 print(len(buoysDict))
-help(constructBuoyDict)
+#help(constructBuoyDict)
 #print(buoySoup)
 #print(type(buoySoup))
 #print(len(buoySoup))
+
+sdLat = 32.72
+sdLon = -117.16
+dLat = 20
+dLon = 20
+
+nearbyBuoys = findNearbyBuoys(buoysDict, sdLat, sdLon, dLat, dLon)
